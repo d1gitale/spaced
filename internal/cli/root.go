494967862d47aecd3fde4f2621dec7c1c77848f8
d1gitale <nkalinin2020@gmail.com>
2026-05-24 21:24:30 +0300

    feat: add flags to check command

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -3,6 +3,7 @@ package cli
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/d1gitale/spaced/internal/domain"
 	"github.com/spf13/cobra"
@@ -13,19 +14,32 @@ type RootCmd struct {
 	repo domain.CardAdapter
 }
 
-func NewRootCmd(ctx context.Context, repo domain.CardAdapter) *RootCmd {
+func NewRootCmd(ctx context.Context, repo domain.CardAdapter) (*RootCmd, error) {
 	rootCmd := &RootCmd{}
 
 	rootCmd.repo = repo
 
 	rootCmd.root = &cobra.Command{
-		Use:   "github.com/d1gitale/spaced",
+		Use:   "spaced",
 		Short: "Spaced repetition CLI",
 		Long:  "spaced is a spaced repetition tracker that helps organizing your schedule and works well with Waybar",
 	}
 
 	listCmd := NewListCmd(repo)
 	listCmd.Flags().String("format", "", "format to print cards in")
+
+	checkCmd := NewCheckCmd(repo)
+	checkCmd.Flags().Int64P("id", "i", 0, "integer id of a card to check")
+	err := checkCmd.MarkFlagRequired("id")
+	if err != nil {
+		return nil, fmt.Errorf("looks like there is a typo: %v", err)
+	}
+
+	checkCmd.Flags().IntP("score", "p", 5, "integer retention score for checked card")
+	err = checkCmd.MarkFlagRequired("score")
+	if err != nil {
+		return nil, fmt.Errorf("looks like there is a typo: %v", err)
+	}
 
 	rootCmd.root.AddCommand(
 		NewAddCmd(repo),
@@ -35,7 +49,7 @@ func NewRootCmd(ctx context.Context, repo domain.CardAdapter) *RootCmd {
 		NewDeleteCmd(repo),
 	)
 
-	return rootCmd
+	return rootCmd, nil
 }
 
 func (rootCmd *RootCmd) Execute(ctx context.Context) error {
