9f1a7db44d1eed85214d6fd9e8c166f2e928a518
d1gitale <nkalinin2020@gmail.com>
2026-05-25 14:26:01 +0300

    fix: temp dir path

--- scripts/install_update_linux.sh
+++ scripts/install_update_linux.sh
@@ -26,7 +26,7 @@ TEMP_DIR=$(mktemp -d)
 trap "rm -rf $TEMP_DIR" EXIT
 
 git clone --depth 1 --branch $BRANCH $REPO_URL "$TEMP_DIR"
-cd "$TEMP_DIR"
+cd "$TEMP_DIR/cmd/app"
 
 CGO_ENABLED=0 go build -ldflags="-s -w" -o "$APP_NAME" .
 
